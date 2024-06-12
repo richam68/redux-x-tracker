f516b1fccdab5c1fb49fffd1fac006cf064c0eab
richam68 <richamhshwr2@gmail.com>
2024-06-12 20:51:38 +0530

    changes done

--- src/utility/validation.js
+++ src/utility/validation.js
@@ -1,20 +1,18 @@
-const validation = (name, totalBudget, category ) => {
-
+const validation = (name, totalBudget, category) => {
   //The total budget must be > 0.
   if (totalBudget < 0) {
-    return {'validate' : false, "message": "Please enter a valid amount"}
+    return { validate: false, message: "Please enter a valid amount" };
   }
 
   //None of the fields must be empty
   if (name === "" || totalBudget === "") {
-   return {'validate' : false, "message": "Please enter a valid amount"}
-   
+    return { validate: false, message: "Please enter a valid amount" };
   }
 
   // Check if any category field is empty
   for (let key in category) {
     if (category[key] === "" || category[key] < 0) {
-     return {'validate' : false, "message": "Please enter a valid amount"}
+      return { validate: false, message: "Please enter a valid amount" };
     }
   }
 
@@ -27,10 +25,10 @@ const validation = (name, totalBudget, category ) => {
   if (categoryWiseSum < totalBudget) {
     category.others += totalBudget - categoryWiseSum;
   } else if (categoryWiseSum > totalBudget) {
-    return {'validate' : false, "message": "Please enter a valid amount"}
+    return { validate: false, message: "Please enter a valid amount" };
   }
 
-  return {'validate': true, "message": "Validate"}
+  return { validate: true, message: "Validate" };
 };
 
 export default validation;
